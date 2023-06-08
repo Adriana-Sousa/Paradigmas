aad191e45fd7c28558cf6af6e40108a9f5bf5ade
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2023-06-07 21:04:28 -0300

    Update Contagem.scm

--- Scheme/Contagem.scm
+++ Scheme/Contagem.scm
@@ -1,6 +1,6 @@
 (define (contar-ocorrencias elemento lista) 
-(cond ((null? lista) 0) ; Caso base: lista vazia, retorna 0
- ((eq? elemento (car lista)) ; Se o elemento é igual ao primeiro elemento da lista 
-(+ 1 (contar-ocorrencias elemento (cdr lista)))) ; Incrementa 1 e chama recursivamente para o restante da lista 
-(else (contar-ocorrencias elemento (cdr lista))))) 
-(display (contar-ocorrencias 'a '(a b c a d a)))
+ (cond ((null? lista) 0) ; Caso base: lista vazia, retorna 0
+      ((eq? elemento (car lista)) ; Se o elemento é igual ao primeiro elemento da lista 
+      (+ 1 (contar-ocorrencias elemento (cdr lista)))) ; Incrementa 1 e chama recursivamente para o restante da lista 
+      (else (contar-ocorrencias elemento (cdr lista))))) 
+(display (contar-ocorrencias 'a '(a b c a d a))) ; exemplo
